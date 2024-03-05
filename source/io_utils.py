0508e1b35fad768b65a0ae428b82b367ed3d014f
Konstantin <ankalagon_black@mail.ru>
2024-03-05 21:42:32 +0300

    Database instead of dataframes

--- source/io_utils.py
+++ source/io_utils.py
@@ -1,4 +1,5 @@
 import os
+import sqlite3
 import pandas as pd
 
 DATA_HASH = 'data_hash'
@@ -12,18 +13,32 @@ PUBLIC_KEY = 'public_key'
 
 ENCRYPTED_KEY = 'encrypted_key'
 
+DATABASE_FILE = 'database.db'
 REGISTRY_FILE = 'registry.csv'
 CUSTOMERS_FILE = 'customers.csv'
 TRANSACTIONS = 'transactions.csv'
+
+FILES_TABLE_STATEMENT = "CREATE TABLE IF NOT EXISTS files (" \
+                        "   id integer PRIMARY KEY," \
+                        "	data_hash text NOT NULL," \
+                        "	account_address text NOT NULL," \
+                        "	short_description text NOT NULL," \
+                        "	iv text NOT NULL," \
+                        "	price integer NOT NULL" \
+                        ")"
+
+INSERT_FILES_STATEMENT = "INSERT OR IGNORE INTO" \
+                               " files(data_hash, account_address, short_description, iv, price)" \
+                               " VALUES(?, ?, ?, ?, ?)"
+
+SELECT_FILES_STATEMENT = "SELECT * from files"
 
 
 class Storage:
 
     def __init__(self, path_to_data_storage):
         self.path_to_data_storage = path_to_data_storage
-        self.registry_path = os.path.join(self.path_to_data_storage, REGISTRY_FILE)
-        self.customers_path = os.path.join(self.path_to_data_storage, CUSTOMERS_FILE)
-        self.transactions_path = os.path.join(self.path_to_data_storage, TRANSACTIONS)
+        self.database_path = os.path.join(self.path_to_data_storage, DATABASE_FILE)
 
     def add_file_to_storage(self, encrypted_data, encrypted_data_hash,
                             user_address):
@@ -37,30 +52,15 @@ class Storage:
         with open(path_to_user_encrypted_data, "wb") as file:
             file.write(encrypted_data)
 
-    def add_record(self, encrypted_data_hash, user_address, short_description, iv,
-                   price):
-        # TODO. Use SQLite instead
-        if not os.path.exists(self.registry_path):
-            data_frame = pd.DataFrame(columns=[DATA_HASH,
-                                               ACCOUNT_ADDRESS,
-                                               SHORT_DESCRIPTION,
-                                               IV,
-                                               PRICE])
+    def add_file(self, encrypted_data_hash, owner_address, short_description, iv, price):
+        conn = open_database(self.database_path)
 
-        else:
-            data_frame = pd.read_csv(self.registry_path, index_col=[0])
+        create_records_table(conn)
 
-        if encrypted_data_hash in data_frame[DATA_HASH].values:
-            data_frame = data_frame.drop(data_frame[data_frame[DATA_HASH] == encrypted_data_hash].index)
+        insert_to_table(conn,
+                        encrypted_data_hash, owner_address, short_description, iv, price)
 
-        data_frame = data_frame.append({DATA_HASH: encrypted_data_hash,
-                                        ACCOUNT_ADDRESS: user_address,
-                                        SHORT_DESCRIPTION: short_description,
-                                        IV: iv.hex(),
-                                        PRICE: price},
-                                       ignore_index=True)
-
-        data_frame.to_csv(self.registry_path)
+        conn.close()
 
     def add_transaction(self, customer_address, data_hash, encrypted_key):
         if not os.path.exists(self.transactions_path):
@@ -86,8 +86,14 @@ class Storage:
     def get_registry_record_by_idx(self, record_idx):
         return pd.read_csv(self.registry_path, index_col=[0]).iloc[record_idx]
 
-    def registry_view(self):
-        return pd.read_csv(self.registry_path, index_col=[0])[[SHORT_DESCRIPTION, PRICE]]
+    def get_files(self):
+        conn = open_database(self.database_path)
+
+        records = select_from_table(conn)
+
+        conn.close()
+
+        return records
 
     def get_registry_records_by_user_address(self, user_address):
         registry = pd.read_csv(self.registry_path, index_col=[0])
@@ -107,6 +113,48 @@ class Storage:
 
     def get_path_to_encrypted_file(self, account_address, data_hash):
         return os.path.join(self.path_to_data_storage, account_address, f"{data_hash}.enc")
+
+
+"""
+Support utils
+"""
+
+
+def open_database(database_path):
+    try:
+        conn = sqlite3.connect(database_path)
+
+        return conn
+
+    except Exception as e:
+        print(e)
+
+
+def create_records_table(conn):
+    cursor = conn.cursor()
+    cursor.execute(FILES_TABLE_STATEMENT)
+    cursor.close()
+
+
+def insert_to_table(conn,
+                    data_hash, account_address, short_description, iv, price):
+    cursor = conn.cursor()
+    cursor.execute(INSERT_FILES_STATEMENT,
+                   (data_hash, account_address, short_description, iv, price))
+    conn.commit()
+    cursor.close()
+
+
+def select_from_table(conn):
+    cursor = conn.cursor()
+
+    cursor.execute(SELECT_FILES_STATEMENT)
+
+    records = cursor.fetchall()
+
+    cursor.close()
+
+    return records
 
 
 # def add_record(self, encrypted_data_hash, tx_hash, user_address, short_description, iv,
@@ -150,3 +198,24 @@ class Storage:
 #                                    ignore_index=True)
 #
 #     data_frame.to_csv(self.customers_path)
+  # if not os.path.exists(self.registry_path):
+        #     data_frame = pd.DataFrame(columns=[DATA_HASH,
+        #                                        ACCOUNT_ADDRESS,
+        #                                        SHORT_DESCRIPTION,
+        #                                        IV,
+        #                                        PRICE])
+        #
+        # else:
+        #     data_frame = pd.read_csv(self.registry_path, index_col=[0])
+
+        # if encrypted_data_hash in data_frame[DATA_HASH].values:
+        #     data_frame = data_frame.drop(data_frame[data_frame[DATA_HASH] == encrypted_data_hash].index)
+        #
+        # data_frame = data_frame.append({DATA_HASH: encrypted_data_hash,
+        #                                 ACCOUNT_ADDRESS: user_address,
+        #                                 SHORT_DESCRIPTION: short_description,
+        #                                 IV: iv.hex(),
+        #                                 PRICE: price},
+        #                                ignore_index=True)
+        #
+        # data_frame.to_csv(self.registry_path)
